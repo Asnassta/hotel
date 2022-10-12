3dfc38b5a88b1a867f648f6b9e2b56e5425d2c84
Asnassta <79850172+Asnassta@users.noreply.github.com>
2022-10-12 17:25:36 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,17 +25,65 @@ $(document).ready(function() {
   });
   /*========/mobile-menu=========*/
 
-  /*=================Sliders===================*/
-  		/*========intro-slider========*/
-  		$('.intro__slider').slick({
-  			fade: true,
-  		  slidesToShow: 1,
-  		  slidesToScroll: 1,
-  		  prevArrow: $('.intro .slider-arrow_prev'),
-  		  nextArrow: $('.intro .slider-arrow_next'),
-  		  dots: true,
-  		});
-  /*=======/intro-slider========*/
+  /*===============Popup=================*/
+    $(".open-popup").on("click", function (event) {
+        name_pop = $(this).attr('data-popup');
+        event.preventDefault();
+        $(".popup."+name_pop).fadeIn(333);
+        $(".popup."+name_pop+" .popup__inner").fadeIn(333);
+        $('body').addClass("hidden");
+    });
+    $(".popup__close,  .closex").on("click", function (event) {
+        event.preventDefault();
+        $(".popup").fadeOut('333');
+        $(".popup__inner").fadeOut(333);
+        $('body').removeClass("hidden");
+    });
+  /*==============/popup=================*/
+
+ /*=================Sliders===================*/
+ 	/*========intro-slider========*/
+  	$('.intro__slider').slick({
+  		fade: true,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		prevArrow: $('.intro .slider-arrow_prev'),
+  		nextArrow: $('.intro .slider-arrow_next'),
+  		dots: true,
+  	});
+	/*=======/intro-slider========*/
+
+	/*========photo-slider========*/
+  	$('.photo-slider').slick({
+  		slidesToShow: 4,
+  		slidesToScroll: 1,
+  		prevArrow: $('.photo .arrow-circle_prev'),
+  		nextArrow: $('.photo .arrow-circle_next'),
+  		responsive: [
+    	 {
+    	   breakpoint: 1400,
+    	    settings: {
+    	        slidesToShow: 3,
+    	        slidesToScroll: 1,
+    	    }
+    	  },
+    	  {
+    	   breakpoint: 769,
+    	    settings: {
+    	        slidesToShow: 2,
+    	        slidesToScroll: 1,
+    	    }
+    	  },
+    	  {
+    	   breakpoint: 547,
+    	    settings: {
+    	        slidesToShow: 1,
+    	        slidesToScroll: 1,
+    	    }
+    	  },
+    	]
+  	});
+	/*=======/photo-slider========*/
 /*=================/sliders===================*/
 });
 
